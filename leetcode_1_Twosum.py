2da3c2e56aff14550d3d010ce6cffda112017bcf
changwoon <sunqloud@gmail.com>
2019-07-27 16:05:24 +0900

    Resolve bug

--- leetcode_1_Twosum.py
+++ leetcode_1_Twosum.py
@@ -1,13 +1,16 @@
 class Solution:
     def twoSum(self, nums, target) :
         ln=len(nums)
+        print(ln)
         for i in range(ln) :
-            for j in range(1, ln-1) :
+            for j in range(i+1, ln) :
                 if ( nums[i]+nums[j] == target) :
-                    return([i,j])
                     # print([i, j])
+                    return([i,j])
 
-a=Solution()
-a.twoSum([2, 7, 11, 15], 9)
+
+# a=Solution()
+# a.twoSum([3,3], 6)
+# a.twoSum([2, 7, 11, 15], 9)
 
 # https://leetcode.com/problems/two-sum/
