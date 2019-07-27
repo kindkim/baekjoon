6c00e28153d4dae96cb7a13a19013927934247ba
changwoon <sunqloud@gmail.com>
2019-07-27 15:54:59 +0900

    Relove Twosum Quiz

--- leetcode_1_Twosum.py
+++ leetcode_1_Twosum.py
@@ -1,3 +1,13 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+    def twoSum(self, nums, target) :
+        ln=len(nums)
+        for i in range(ln) :
+            for j in range(1, ln-1) :
+                if ( nums[i]+nums[j] == target) :
+                    return([i,j])
+                    # print([i, j])
+
+a=Solution()
+a.twoSum([2, 7, 11, 15], 9)
+
+# https://leetcode.com/problems/two-sum/
